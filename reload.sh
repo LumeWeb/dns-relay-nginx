5c569e3286fec5dc91d71149ce8c09b58fdd8fa5
Derrick Hammer <derrick@derrickhammer.com>
2022-04-19 07:52:45 -0400

    *Add message after ssl is updated

--- reload.sh
+++ reload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-while true
-do
- inotifywait -e create -e modify  --include ".reload" /etc/letsencrypt
+while true; do
+  inotifywait -e create -e modify --include ".reload" /etc/letsencrypt
   /usr/local/openresty/bin/openresty -s reload
+  echo "SSL Certificate Updated"
   rm -f /etc/letsencrypt/.reload
 done
